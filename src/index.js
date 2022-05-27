91d775c5171fca6a2a70c91883636701f496bd2a
Hanka Milotova <hanka.esha2@gmail.com>
2022-05-27 17:12:06 +0200

    - BEM logic applied to navigation and menu components - Javascript logic improved to be more universal

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 new Splide("#promo-carousel").mount();
 new Splide("#hot-tips-carousel").mount();
-new Splide("#slider3").mount();
+new Splide("#articles-carousel").mount();
 
-document.querySelector(".menu-icon").addEventListener("click", () => {
-    document.querySelector(".menu").classList.toggle("menu-visible");
-});
-
-document.querySelector("#menu-clothes").addEventListener("click", () => {
-    document.querySelector("#menu-clothes-list").classList.toggle("category-list-visible");
-});
-document.querySelector("#menu-skis").addEventListener("click", () => {
-    document.querySelector("#menu-skis-list").classList.toggle("category-list-visible");
-});
-document.querySelector("#menu-bikes").addEventListener("click", () => {
-    document.querySelector("#menu-bikes-list").classList.toggle("category-list-visible");
+document.querySelector(".navbar--menu--icon").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("menu--visible");
 });
 
-document.querySelector("#clothes").addEventListener("click", () => {
-    document.querySelector("#clothes-list").classList.toggle("category-list-visible");
-});
-document.querySelector("#skis").addEventListener("click", () => {
-    document.querySelector("#skis-list").classList.toggle("category-list-visible");
-});
-document.querySelector("#bikes").addEventListener("click", () => {
-    document.querySelector("#bikes-list").classList.toggle("category-list-visible");
+document.querySelectorAll(".menu__category, .category--button").forEach((element) => {
+    element.addEventListener("click", (event) => {
+        event.target.closest(".menu__item, .category").classList.toggle("category__list--visible");
+    });
 });
