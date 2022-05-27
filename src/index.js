73274e64aeeaa3604a657204cd0a901ec420b789
Hanka Milotova <hanka.esha2@gmail.com>
2022-05-27 20:47:16 +0200

    navbar and category menu components created, BEM improved, top page carousel code fine-tuned

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ new Splide("#promo-carousel").mount();
 new Splide("#hot-tips-carousel").mount();
 new Splide("#articles-carousel").mount();
 
-document.querySelector(".navbar--menu--icon").addEventListener("click", () => {
+document.querySelector(".navbar__menu-icon").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("menu--visible");
 });
 
-document.querySelectorAll(".menu__category, .category--button").forEach((element) => {
+document.querySelectorAll(".category__header, .category__button").forEach((element) => {
     element.addEventListener("click", (event) => {
-        event.target.closest(".menu__item, .category").classList.toggle("category__list--visible");
+        event.target.closest(".category, .category").classList.toggle("category__list--visible");
     });
 });
