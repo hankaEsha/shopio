eb1758fc86f01f6344cdcc14045a2dcd1f7566a2
Hanka Milotova <hanka.esha2@gmail.com>
2022-05-30 23:21:39 +0200

    Updates

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ new Splide("#promo-carousel").mount();
 new Splide("#hot-tips-carousel").mount();
 new Splide("#articles-carousel").mount();
 
-document.querySelector(".menu-icon").addEventListener("click", () => {
-    document.querySelector(".menu").classList.toggle("menu-visible");
+document.querySelector(".navbar__menu-icon").addEventListener("click", () => {
+    document.querySelector(".menu").classList.toggle("menu--visible");
 });
 
-document.querySelectorAll(".menu-category, .category-button").forEach((element) => {
+document.querySelectorAll(".category__header, .category__button").forEach((element) => {
     element.addEventListener("click", (event) => {
-        event.target.closest(".menu-item, .category").classList.toggle("category-list-visible");
+        event.target.closest(".category").classList.toggle("category__list--visible");
     });
 });
+
