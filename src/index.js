acd7c3acf5fcf28255131c9ba54776517f11a7da
Hanka Milotova <hanka.esha2@gmail.com>
2022-05-30 04:06:50 +0200

    footer, mailing and benefit icons section updated, bugs fixed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,30 @@ new Splide("#promo-carousel").mount();
 new Splide("#hot-tips-carousel").mount();
 new Splide("#articles-carousel").mount();
 
+// const categoryListVisible = (screenWidth) => {
+//     if (screenWidth.matches) {
+//         // If media query matches
+//         document.querySelector(".category").classList.add("category__list--visible");
+//     } else {
+//         document.querySelectorAll(".category__header, .category__button").forEach((element) => {
+//             element.addEventListener("click", (event) => {
+//                 event.target.closest(".category").classList.toggle("category__list--visible");
+//             });
+//         });
+//     }
+// };
+
+// let screenWidth = window.matchMedia("(max-width: 768px)");
+// categoryListVisible(screenWidth); // Call listener function at run time
+// screenWidth.addEventListener(myFunction); // Attach listener function on state changes
+
 document.querySelector(".navbar__menu-icon").addEventListener("click", () => {
     document.querySelector(".menu").classList.toggle("menu--visible");
 });
 
 document.querySelectorAll(".category__header, .category__button").forEach((element) => {
     element.addEventListener("click", (event) => {
-        event.target.closest(".category, .category").classList.toggle("category__list--visible");
+        event.target.closest(".category").classList.toggle("category__list--visible");
     });
 });
+
